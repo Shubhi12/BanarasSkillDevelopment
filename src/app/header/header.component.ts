459847d116ce688c9901ad6d5547eca893235608
Shubhi12 <saloni.sahu@agribazaar.com>
2020-07-18 15:02:05 +0530

    7th commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import { from } from 'rxjs';
-
+declare var $ : any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -11,6 +11,14 @@ export class HeaderComponent implements OnInit {
   constructor(public router:Router) { }
 
   ngOnInit() {
+    $(function(){ 
+      var navMain = $(".navbar-collapse"); // avoid dependency on #id
+      // "a:not([data-toggle])" - to avoid issues caused
+      // when you have dropdown inside navbar
+      navMain.on("click", "a:not([data-toggle])", null, function () {
+          navMain.collapse('hide');
+      });
+  });
   }
 
 }
